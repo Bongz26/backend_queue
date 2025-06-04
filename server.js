21baf8835b69c9ba2da44f346708db3fe5ac817d
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-04 10:56:33 +0200

    adding employee verification

--- server.js
+++ server.js
@@ -189,6 +189,24 @@ app.post("/api/orders", async (req, res) => {
     }
 });
 
+//verify employee
+app.get("/api/employees", async (req, res) => {
+    try {
+        const { code } = req.query;
+
+        const result = await pool.query("SELECT employee_name FROM employees WHERE employee_code = $1", [code]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "❌ Invalid Employee Code!" });
+        }
+
+        res.json({ employee_name: result.rows[0].employee_name });
+    } catch (error) {
+        console.error("🚨 Error fetching employee:", error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.get("/", (req, res) => {
   res.send("🚀 Backend is alive! 🫠 ");
 });
