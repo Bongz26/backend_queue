9452db6f929cc60c88ba4019731a316ba19d4339
Bongz <106278406+Bongz26@users.noreply.github.com>
2025-06-09 11:55:46 +0200

    Updated CORS Settings

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "*",
+    origin: "https://queue-system-ewrn.onrender.com",
     methods: ["GET", "POST", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
